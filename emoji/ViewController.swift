5a90e7ac0108e40f579fc2d3c0b59adb4e005c2f
Dhruvil <dhruvilshah35@gmail.com>
2020-01-28 09:50:03 -0800

    Initial Commit

---
+++ emoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emoji
+//
+//  Created by Dhruvil on 1/28/20.
+//  Copyright © 2020 Dhruvil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
